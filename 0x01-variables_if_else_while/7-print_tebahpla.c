f06c4d3b2df600ea0d262766d812dfbf9999a8cc
RayanKW <wairimurayya@gmail.com>
2022-09-09 14:25:47 -0700

    smiley

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-
-
 /**
  * main - The main function is our entry point
  *
@@ -11,25 +8,14 @@
  *
  * Return: Always 0 (Success)
  */
-
-
-
 int main(void)
-
 {
-
 	char low;
-
-
 
 	for (low = 'z'; low >= 'a'; low--)
 
 		putchar(low);
 
 		putchar('\n');
-
-
-
-	return (0);
-
+	return (0)
 }
