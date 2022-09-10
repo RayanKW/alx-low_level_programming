3f8b58da07f62d21f3b7c8db73ecfb7fbbc27f6b
RayanKW <wairimurayya@gmail.com>
2022-09-10 00:08:58 -0700

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,34 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-
 /**
- * main - The msin function is our entry point
+ * main - The main function is our entry point
  *
  * The program prints out aphabets in lowercase
  *
  * Return: Always 0 (Success)
  */
-
-
-
 int main(void)
-
 {
-
 	char low;
-
-
-
-	for (low = 'a'; low <= 'z'; low++)
-
-		putchar(low);
+	for (low = 'a'; low <= 'z'; low++) putchar(low);
 
 		putchar('\n');
-
-
-
 	return (0);
-
 }
