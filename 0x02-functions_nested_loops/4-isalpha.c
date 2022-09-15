2f3b1b7553960b0a6380eb84623f621f8abe6d33
RayanKW <wairimurayyan@gmail.com>
2022-09-15 14:24:20 +0300

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -13,14 +13,10 @@
  */
 int _isalpha(int c)
 {
-	char lower, upper;
-	int isletter = 0;
-
-	for (lower = 'a'; lower <= 'z'; lower++)
+	if ((c > 'a' && c <= 'z') || >= 'A' && c <= 'Z'))
 	{
-		for (upper = 'A'; upper <= 'Z'; upper++)
-		{
-			if(c == 'A' || c == upper)
-			isletter = 1;
+		return (1);
 	}
+	else
+		return (0);
 }
